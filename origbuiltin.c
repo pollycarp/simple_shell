4f1802c530d833d4faa49768384020b4d0a0f287
Njiraini23 <njiraini88@gmail.com>
2023-01-22 11:17:02 -0800

    checkers

--- origbuiltin.c
+++ origbuiltin.c
@@ -169,6 +169,8 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front)
  */
 int shellby_help(char **args, char __attribute__((__unused__)) **front)
 {
+	char name;
+
 	if (!args[0])
 		help_all();
 	else if (_strcmp(args[0], "alias") == 0)
