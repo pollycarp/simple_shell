08840a1c70fa17a6ba1082190e0fdf3c432932b8
Njiraini23 <njiraini88@gmail.com>
2023-02-23 11:26:55 +0000

    realloc.c revised

--- realloc.c
+++ realloc.c
@@ -1,42 +1,22 @@
 #include "shell.h"
-
 /**
- *_malloc -allocated memory for nmemb elemn of size bytes
- *@nmemb: number of element in the array
- *@size: bytes for each position in the array
- *Return: void
+ * _getline_command -  GEts inputs
+ * Return: The input.
  */
-void *_malloc(unsigned int nmemb, unsigned int size)
+
+char *_getline_command(void)
 {
-	char *p;
-	unsigned int i;
+	char *lineptr = NULL;
+	size_t charter_user = 0;
 
-	if (nmemb == 0 || size == 0)
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+
+	if (getline(&lineptr, &charter_user, stdin) == -1)
+	{
+		free(lineptr);
 		return (NULL);
+	}
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-
-	for (i = 0; i < nmemb * size; i++)
-		p[i] = 0;
-
-	return (p);
-
-}
-
-/**
- * _memset - fills memory with a constant byte
- * @s: the pointer to the memory area
- * @b: the byte to fill *s with
- * @n: the amount of bytes to be filled
- * Return: (s) a pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
+	return (lineptr);
 }
