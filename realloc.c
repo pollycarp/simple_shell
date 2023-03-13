faa3eb3348bb627b37264dcc70c3f808d10dc8b4
Njiraini23 <njiraini88@gmail.com>
2023-03-13 15:00:45 +0000

    realloc.c

--- realloc.c
+++ realloc.c
@@ -1,22 +1,62 @@
 #include "shell.h"
+
 /**
- * _getline_command -  GEts inputs
- * Return: The input.
+ **_memset - fills memory with a constant byte
+ *@s: the pointer to the memory area
+ *@b: the byte to fill *s with
+ *@n: the amount of bytes to be filled
+ *Return: (s) a pointer to the memory area s
  */
-
-char *_getline_command(void)
+char *_memset(char *s, char b, unsigned int n)
 {
-	char *lineptr = NULL;
-	size_t charter_user = 0;
+	unsigned int i;
 
-	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "$ ", 2);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
 
-	if (getline(&lineptr, &charter_user, stdin) == -1)
-	{
-		free(lineptr);
+/**
+ * ffree - frees a string of strings
+ * @pp: string of strings
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ *
+ * Return: pointer to da ol'block nameen.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
 		return (NULL);
-	}
 
-	return (lineptr);
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
 }
