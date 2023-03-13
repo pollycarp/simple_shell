6fe9336861a8d7a31b2206cc988b7d0a26650d3c
Njiraini23 <njiraini88@gmail.com>
2023-03-13 14:48:02 +0000

    enviro.c

--- enviro.c
+++ enviro.c
@@ -1,138 +1,93 @@
 #include "shell.h"
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
-int lsh_ctrld(char **args);
-
-/*
- * List of builtin commands, followed by their corresponding functions.
- */
-char *builtin_str[] = {"cd", "help", "exit", "^D"};
-
-int (*builtin_func[]) (char **) = {&lsh_cd, &lsh_help, &lsh_exit, &lsh_ctrld};
 
 /**
- * lsh_num_builtins - size
- * Return: size
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
-
-int lsh_num_builtins(void)
+int _myenv(info_t *info)
 {
-	return (sizeof(builtin_str) / sizeof(char *));
+	print_list_str(info->env);
+	return (0);
 }
 
-/*
- * Builtin function implementations.
-*/
+/**
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
 
 /**
- * lsh_cd - builtin to change dirs
- * @args: List of args.  args[0] is "cd".  args[1] is the directory.
- * Return: 1 on success
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
  */
-int lsh_cd(char **args)
+int _mysetenv(info_t *info)
 {
-	if (args[1] == NULL)
+	if (info->argc != 3)
 	{
-		fprintf(stderr, "hsh: expected argument to \"cd\"\n");
+		_eputs("Incorrect number of arguements\n");
+		return (1);
 	}
-	else
-	{
-		if (chdir(args[1]) != 0)
-		{
-			perror("hsh");
-		}
-	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
 	return (1);
 }
 
 /**
- * lsh_help - prints the help for the shell
- * @args: List of args.  Not examined.
- * Return: Always returns 1, to continue executing.
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
  */
-int lsh_help(char **args)
+int _myunsetenv(info_t *info)
 {
 	int i;
 
-	printf("Oscar Bedat and Andres Henderson\n");
-	printf("If you need help, call 1-800-help\n");
-	(void)args;
-	for (i = 0; i < lsh_num_builtins(); i++)
+	if (info->argc == 1)
 	{
-		printf("  %s\n", builtin_str[i]);
-	}
-
-	return (1);
-}
-
-/**
- * lsh_exit - builtin to exit the shell
- * @args: List of args.  Not examined.
- * Return: Always returns 0, to terminate execution.
- */
-int lsh_exit(char **args)
-{
-	(void)args;
-	free(args);
-	return (200);
-}
-
-/**
- * lsh_ctrld - builtin to handle "^D" call
- * @args: List of args.  Not examined.
- * Return: Always returns 0, to terminate execution.
- */
-int lsh_ctrld(char **args)
-{
-	(void)args;
-	free(args);
-	return (200);
-}
-
-/**
- *_fork_fun - foo that create a fork.
- *@arg: Command and values path.
- *@av: Has the name of our program.
- *@env: Environment
- *@lineptr: Command line for the user.
- *@np: ID of proces.
- *@c: Checker add new test
- *Return: 0 success
- */
-
-int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c)
-{
-
-	pid_t child;
-	int status, i = 0;
-	char *format = "%s: %d: %s: not found\n";
-
-	if (arg[0] == NULL)
+		_eputs("Too few arguements.\n");
 		return (1);
-	for (i = 0; i < lsh_num_builtins(); i++)
-	{
-		if (_strcmp(arg[0], builtin_str[i]) == 0)
-			return (builtin_func[i](arg));
 	}
-	child = fork();
-	if (child == 0)
-	{
-		if (execve(arg[0], arg, env) == -1)
-		{
-			fprintf(stderr, format, av[0], np, arg[0]);
-			if (!c)
-				free(arg[0]);
-			free(arg);
-			free(lineptr);
-			exit(errno);
-		}
-	}
-	else
-	{
-		wait(&status);
-		return (status);
-	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
 	return (0);
 }
 
