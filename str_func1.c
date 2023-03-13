f9baa712655e11fb7eed2d978c01ca40425f873f
Njiraini23 <njiraini88@gmail.com>
2023-03-13 15:05:28 +0000

    str_func1.c

--- str_func1.c
+++ str_func1.c
@@ -1,113 +1,87 @@
 #include "shell.h"
 
 /**
- * _strcat - ncatenates two strings
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
  *
- * @dest: Destination string
- *
- * @src: Origin string
- * Return: Always 0
+ * Return: pointer to destination
  */
-
-
-
-char *_strcat(char *dest, char *src)
-{
-	int i, j = 0;
-
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
-	for (j = 0; src[j] != '\0'; j++)
-	{
-		dest[i] = src[j];
-		i++;
-
-	}
-	dest[i] = '\0';
-return (dest);
-}
-
-
-
-/**
- * _strcmp - Compares two strings
- *
- * @s1: a string
- * @s2: Another string
- *
- * Return: multiple returns
- *
- */
-
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
-	int val;
-
-	while (s1[i] != 0 && s2[i] != 0 && s1[i] == s2[i])
-		i++;
-val = s1[i] - s2[i];
-return (val);
-
-}
-
-/**
- * _strlen - gets the size of a string
- * @s: the string to measure
- * Return: the length of the string
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != 0; i++)
-		;
-
-	return (i);
-}
-
-/**
- *_strncmp -  function that compares two strings.
- *@s1: string one
- *@s2: string two
- *@n: number of characters
- * Return: diference
- */
-
-size_t _strncmp(char *s1, char *s2, size_t n)
-{
-	size_t i, j;
-
-	for (j = 0; s1[j] != '\0' && j < n; j++)
-	{
-		i = s1[j] - s2[j];
-
-		if (i != 0)
-		{
-			return (i);
-		}
-	}
-	return (0);
-}
-
-/**
- * _strcpy - copies the string pointed to by src into dest
- * @dest: destination of the copy
- * @src: source of the copy
- *
- * Return: char pointer to dest
- */
-
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
 	{
 		dest[i] = src[i];
+		i++;
 	}
-	dest[i + 1] = 0;
+	dest[i] = 0;
 	return (dest);
+}
+
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ *_puts - prints an input string
+ *@str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
