f920766dc6dbd3ef1ecd4013da9e5e5a259d3496
Njiraini23 <njiraini88@gmail.com>
2023-03-13 15:10:49 +0000

    str_funct.c

--- str_funct.c
+++ str_funct.c
@@ -1,51 +1,75 @@
 #include "shell.h"
+
 /**
- * main - Main arguments functions
- * @ac: Count of argumnents
- * @av: Arguments
- * @env: Environment
- * Return: _exit = 0.
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
  */
-int main(int ac, char **av, char **env)
+int _strlen(char *s)
 {
-	int pathValue = 0, status = 0, is_path = 0;
-	char *line = NULL, /**ptr to inpt*/ **commands = NULL; /**tokenized commands*/
-	(void)ac;
-	while (1)/* loop until exit */
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - performs lexicogarphic comparison of two strangs.
+ * @s1: the first strang
+ * @s2: the second strang
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
 	{
-		errno = 0;
-		line = _getline_command();/** reads user input*/
-		if (line == NULL && errno == 0)
-			return (0);
-		if (line)
-		{
-			pathValue++;
-			commands = tokenize(line);/** tokenizes or parse user input*/
-			if (!commands)
-				free(line);
-			if (!_strcmp(commands[0], "env"))/**checks if user wrote env"*/
-				_getenv(env);
-			else
-			{
-				is_path = _values_path(&commands[0], env);/** tokenizes PATH*/
-				status = _fork_fun(commands, av, env, line, pathValue, is_path);
-					if (status == 200)
-					{
-						free(line);
-						return (0);
-					}
-				if (is_path == 0)
-					free(commands[0]);
-			}
-			free(commands); /*free up memory*/
-		}
-		else
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);/** Writes to standard output*/
-			exit(status);
-		}
-		free(line);
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
 	}
-	return (status);
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ *
+ * Return: address of next char of haystack or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
 }
